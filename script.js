1753dd5a0188c7b023c261145e9c03b8a17a8498
deey <adeniyiadedayo95@yahoo.com>
2020-10-13 12:24:48 +0100

    third

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const dateToday = document.querySelector('#date');
 let d = new Date();
 const days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+const months = ["January","Febuary","March","April","May","June","July", "August", "September", "October", "November", "December"];
 
-dateToday.textContent = `${days[d.getDay()]}, ${d.getDay()}-${d.getMonth()}-${d.getFullYear()}`;
+dateToday.textContent = `${days[d.getDay()]}, ${months[d.getMonth()]} ${d.getDay()}, ${d.getFullYear()}`;
